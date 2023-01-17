9c0f04c2c49437f498c3ed8aa861a68aa2cdcf01
p_eichhorn2 <m.eichhorn@schwarz-produktion.com>
2023-01-17 16:45:09 +0100

    fix: counter values

--- exporters/metrics_exporter/metrics.go
+++ exporters/metrics_exporter/metrics.go
@@ -11,12 +11,16 @@ import (
 var custom_gauge *prometheus.GaugeVec
 var custom_counter *prometheus.CounterVec
 var Failed_requests *prometheus.CounterVec
+var last_Count float64
+var diff float64
 
 func ExposeMetrics(namespace string) {
 
 	custom_gauge = promauto.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "custom_gauge_metric", Help: "Metric for collecting gopclog gauge type tag values"}, []string{"NodeId", "NodeName"})
 	custom_counter = promauto.NewCounterVec(prometheus.CounterOpts{Namespace: namespace, Name: "custom_counter_metric", Help: "Metric for collecting gopclog counter type tag values"}, []string{"NodeId", "NodeName"})
 	Failed_requests = promauto.NewCounterVec(prometheus.CounterOpts{Namespace: namespace, Name: "number_failed_request", Help: "Metric for collecting gopclog number of failed request to the specified target URL"}, []string{"url"})
+	last_Count = 0
+	diff = 0
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":4444", nil)
@@ -30,7 +34,9 @@ func SetMetricsValue(metricsType string, nodeId string, tagname string, tagValue
 	}
 
 	if metricsType == "Counter" {
-		custom_counter.WithLabelValues(nodeId, tagname).Add(tagValue)
+		diff = tagValue - last_Count
+		custom_counter.WithLabelValues(nodeId, tagname).Add(diff)
+		last_Count = tagValue
 	}
 
 }
