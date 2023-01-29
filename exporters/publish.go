b87706269b40cea4eaf975e261c93091ce571123
Max Eichhorn <m.3ichhorn@gmail.com>
2023-01-29 20:23:38 +0100

    feat: granular exporter config

--- exporters/publish.go
+++ exporters/publish.go
@@ -1,12 +1,19 @@
 package exporter
 
 import (
+	"strings"
 	"time"
 
-	"github.com/doteich/OPC-UA-Logger/exporters/http_exporter"
+	"github.com/doteich/OPC-UA-Logger/exporters/db_exporter"
 	"github.com/doteich/OPC-UA-Logger/exporters/metrics_exporter"
 	"github.com/doteich/OPC-UA-Logger/setup"
 )
+
+var SelectedExportes []string
+
+func setExporters() {
+
+}
 
 func PublishData(nodeId string, iface interface{}, timestamp time.Time) {
 
@@ -15,55 +22,50 @@ func PublishData(nodeId string, iface interface{}, timestamp time.Time) {
 	for _, node := range config.Nodes {
 		if node.NodeId == nodeId {
 
-			var dataType string
-			var metricsValue float64
+			//http_exporter.PostLoggedData(node.NodeId, node.NodeName, iface, timestamp, config.LoggerConfig.Name, config.ClientConfig.Url)
+			namespace := strings.Replace(config.LoggerConfig.Name, " ", "", -1)
+			db_exporter.InsertValues(namespace, node.NodeId, node.NodeName, iface, timestamp, config.LoggerConfig.Name, config.ClientConfig.Url)
 
-			switch v := iface.(type) {
-			case int:
-				dataType = "int"
-				metricsValue = float64(v)
-			case int8:
-				dataType = "int8"
-				metricsValue = float64(v)
-			case int16:
-				dataType = "int16"
-				metricsValue = float64(v)
-			case int32:
-				dataType = "int32"
-				metricsValue = float64(v)
-			case uint8:
-				dataType = "uint8"
-				metricsValue = float64(v)
-			case uint16:
-				dataType = "uint16"
-				metricsValue = float64(v)
-			case uint32:
-				dataType = "uint32"
-				metricsValue = float64(v)
-			case float32:
-				dataType = "float32"
-				metricsValue = float64(v)
-			case float64:
-				dataType = "float64"
-				metricsValue = v
-			case string:
-				dataType = "string"
-			case bool:
-				dataType = "bool"
-			}
-
-			http_exporter.PostLoggedData(node.NodeId, node.NodeName, iface, timestamp, config.LoggerConfig.Name, config.ClientConfig.Url, dataType)
-
-			if config.LoggerConfig.MetricsEnabled && (dataType != "bool" || dataType != "string") {
-				ExportMetric(node.MetricsType, node.NodeId, node.NodeName, metricsValue)
+			if config.LoggerConfig.MetricsEnabled {
+				ExportMetric(node.MetricsType, node.NodeId, node.NodeName, iface)
 			}
 		}
 	}
 
 }
 
-func ExportMetric(metricsType string, nodeId string, name string, metricsValue float64) {
+func ExportMetric(metricsType string, nodeId string, name string, iface interface{}) {
 
-	metrics_exporter.SetMetricsValue(metricsType, nodeId, name, metricsValue)
+	switch v := iface.(type) {
+	case int:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case int8:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case int16:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case int32:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case uint:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case uint8:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case uint16:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case uint32:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case float32:
+		value := float64(v)
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, value)
+	case float64:
+		metrics_exporter.SetMetricsValue(metricsType, nodeId, name, v)
+	}
 
 }
