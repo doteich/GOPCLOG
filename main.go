d854c9c25761de7b1a06bb21b40589dc1a672fbf
Max Eichhorn <m.3ichhorn@gmail.com>
2023-02-02 21:25:32 +0100

    feat: changes to structure

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"os/signal"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/doteich/OPC-UA-Logger/exporters/db_exporter"
 	"github.com/doteich/OPC-UA-Logger/exporters/http_exporter"
@@ -15,7 +9,6 @@ import (
 	"github.com/doteich/OPC-UA-Logger/exporters/metrics_exporter"
 	"github.com/doteich/OPC-UA-Logger/machine/opcua_monitor"
 	"github.com/doteich/OPC-UA-Logger/setup"
-	"github.com/gopcua/opcua/monitor"
 )
 
 func main() {
@@ -27,6 +20,7 @@ func main() {
 	}
 
 	namespace := strings.Replace(config.LoggerConfig.Name, " ", "", -1)
+
 	db_exporter.SetupDBConnection(namespace)
 
 	http_exporter.InitRoutes(config.LoggerConfig.TargetURL)
@@ -37,50 +31,8 @@ func main() {
 		go metrics_exporter.ExposeMetrics(namespace)
 	}
 
+	opcua_monitor.CreateOPCUAMonitor(config)
+
 	//exporter.ReadLogFile()
-
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, os.Interrupt)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	go func() {
-		<-signalCh
-		println()
-		cancel()
-	}()
-
-	ep := opcua_monitor.ValidateEndpoint(ctx, config.ClientConfig.Url, config.ClientConfig.SecurityPolicy, config.ClientConfig.SecurityMode)
-
-	connectionParams := opcua_monitor.SetClientOptions(&config, ep)
-
-	client := opcua_monitor.CreateClientConnection(config.ClientConfig.Url, connectionParams)
-	err := client.Connect(ctx)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer client.CloseSessionWithContext(ctx)
-
-	nodeMonitor, err := monitor.NewNodeMonitor(client)
-
-	if err != nil {
-		panic("Failed to setup monitor")
-	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go opcua_monitor.MonitorItems(ctx, nodeMonitor, time.Duration(config.LoggerConfig.Interval*1000000000), 1000, wg, config.Nodes)
-
-	<-ctx.Done()
-	defer ShowDone()
-
-}
-
-func ShowDone() {
-	fmt.Println("ABORTING")
 
 }
