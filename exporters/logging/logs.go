52d7807b0dce47da242fef864badf5425f2fde89
Max Eichhorn <m.3ichhorn@gmail.com>
2023-03-02 20:13:14 +0100

    feat: changes to logs

--- exporters/logging/logs.go
+++ exporters/logging/logs.go
@@ -1,55 +1,62 @@
 package logging
 
 import (
-	"fmt"
 	"os"
 
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+	"golang.org/x/exp/slog"
 )
 
-var Logs *zap.Logger
-var Buffer *zap.Logger
+var (
+	Logger *slog.TextHandler
+)
 
-func InitLogs() {
-	logConfig := zap.NewProductionEncoderConfig()
+// var Logs *zap.Logger
+// var Buffer *zap.Logger
 
-	logConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	logConfig.LineEnding = ","
-	fileEncoder := zapcore.NewJSONEncoder(logConfig)
-	consoleEncoder := zapcore.NewConsoleEncoder(logConfig)
+// func InitLogs() {
+// 	logConfig := zap.NewProductionEncoderConfig()
 
-	bufferConfig := zapcore.EncoderConfig{
-		MessageKey: "msg",
-		LineEnding: ",",
-	}
+// 	logConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+// 	logConfig.LineEnding = ","
+// 	fileEncoder := zapcore.NewJSONEncoder(logConfig)
+// 	consoleEncoder := zapcore.NewConsoleEncoder(logConfig)
 
-	bufferEncoder := zapcore.NewJSONEncoder(bufferConfig)
+// 	bufferConfig := zapcore.EncoderConfig{
+// 		MessageKey: "msg",
+// 		LineEnding: ",",
+// 	}
 
-	_, err0 := os.Stat("./tmp")
+// 	bufferEncoder := zapcore.NewJSONEncoder(bufferConfig)
 
-	if err0 != nil {
-		os.MkdirAll("tmp/logs", 0777)
-	}
+// 	_, err0 := os.Stat("./tmp")
 
-	logFile, err := os.OpenFile("tmp/logs/log.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+// 	if err0 != nil {
+// 		os.MkdirAll("tmp/logs", 0777)
+// 	}
 
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	logFile, err := os.OpenFile("tmp/logs/log.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 
-	bufferFile, err := os.OpenFile("tmp/logs/buffer.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	defaultLogLevel := zapcore.ErrorLevel
-	core := zapcore.NewTee(
-		zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), defaultLogLevel),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
-	)
+// 	bufferFile, err := os.OpenFile("tmp/logs/buffer.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 
-	bufferCore := zapcore.NewTee(
-		zapcore.NewCore(bufferEncoder, zapcore.AddSync(bufferFile), defaultLogLevel))
+// 	defaultLogLevel := zapcore.ErrorLevel
+// 	core := zapcore.NewTee(
+// 		zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), defaultLogLevel),
+// 		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
+// 	)
 
-	Logs = zap.New(core, zap.AddStacktrace(zapcore.ErrorLevel))
-	Buffer = zap.New(bufferCore, zap.AddStacktrace(zapcore.ErrorLevel))
+// 	bufferCore := zapcore.NewTee(
+// 		zapcore.NewCore(bufferEncoder, zapcore.AddSync(bufferFile), defaultLogLevel))
 
+// 	Logs = zap.New(core, zap.AddStacktrace(zapcore.ErrorLevel))
+// 	Buffer = zap.New(bufferCore, zap.AddStacktrace(zapcore.ErrorLevel))
+
+// }
+
+func InitLogger() {
+	logOpts := slog.HandlerOptions{Level: slog.LevelInfo}
+	Logger = logOpts.NewTextHandler(os.Stdout)
 }
