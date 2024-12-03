c870487c616b7de09a2351f4dc695a31fa167a48
m.eichhorn <m.eichhorn@schwarz-produktion.com>
2024-12-03 09:12:41 +0100

    feat: added connection monitoring and reconnect

--- exporters/metrics_exporter/metrics.go
+++ exporters/metrics_exporter/metrics.go
@@ -9,6 +9,7 @@ var custom_gauge *prometheus.GaugeVec
 var custom_counter *prometheus.GaugeVec
 var custom_technical_counter *prometheus.CounterVec
 var Failed_requests *prometheus.CounterVec
+var opcua_connects *prometheus.CounterVec
 
 func ExposeMetrics(namespace string) {
 
@@ -16,7 +17,7 @@ func ExposeMetrics(namespace string) {
 	custom_counter = promauto.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "custom_counter_metric", Help: "Metric for collecting gopclog counter type tag values"}, []string{"NodeId", "NodeName"})
 	custom_technical_counter = promauto.NewCounterVec(prometheus.CounterOpts{Namespace: namespace, Name: "custom_technical_counter_metric", Help: "Metric for collecting gopclog technical counter type tag values"}, []string{"NodeId", "NodeName"})
 	Failed_requests = promauto.NewCounterVec(prometheus.CounterOpts{Namespace: namespace, Name: "number_failed_request", Help: "Metric for collecting gopclog number of failed request to the specified target URL"}, []string{"url"})
-
+	opcua_connects = promauto.NewCounterVec(prometheus.CounterOpts{Namespace: namespace, Name: "number_opcua_connection_attempts", Help: "Metric for collecting gopclog number of failed request to the specified target URL"}, []string{"server"})
 }
 
 func SetMetricsValue(metricsType string, nodeId string, tagname string, tagValue float64) {
@@ -33,4 +34,8 @@ func SetMetricsValue(metricsType string, nodeId string, tagname string, tagValue
 		custom_technical_counter.WithLabelValues(nodeId, tagname).Add(tagValue)
 	}
 
+}
+
+func LogReconnects(s string) {
+	opcua_connects.WithLabelValues(s).Add(1)
 }
